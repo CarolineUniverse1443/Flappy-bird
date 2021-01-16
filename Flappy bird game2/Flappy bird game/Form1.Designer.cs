878d4416213be4d80881f30c67ba9e023d231f13
Caroline Popova <54083037+CarolineUniverse1443@users.noreply.github.com>
2021-01-17 00:36:35 +0300

    Add files via upload

---
+++ Flappy bird game2/Flappy bird game/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Flappy_bird_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.p1 = new System.Windows.Forms.PictureBox();
+            this.p2 = new System.Windows.Forms.PictureBox();
+            this.flappy = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.flappy)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ground
+            // 
+            this.ground.BackgroundImage = global::Flappy_bird_game.Properties.Resources.Ground;
+            this.ground.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.ground.Location = new System.Drawing.Point(0, 350);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(618, 65);
+            this.ground.TabIndex = 0;
+            this.ground.TabStop = false;
+            // 
+            // p1
+            // 
+            this.p1.BackColor = System.Drawing.Color.Transparent;
+            this.p1.BackgroundImage = global::Flappy_bird_game.Properties.Resources.pipedown;
+            this.p1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.p1.Location = new System.Drawing.Point(328, -2);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(77, 116);
+            this.p1.TabIndex = 1;
+            this.p1.TabStop = false;
+            // 
+            // p2
+            // 
+            this.p2.BackColor = System.Drawing.Color.Transparent;
+            this.p2.BackgroundImage = global::Flappy_bird_game.Properties.Resources.pipe;
+            this.p2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.p2.Location = new System.Drawing.Point(328, 237);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(77, 116);
+            this.p2.TabIndex = 2;
+            this.p2.TabStop = false;
+            // 
+            // flappy
+            // 
+            this.flappy.BackColor = System.Drawing.Color.Transparent;
+            this.flappy.BackgroundImage = global::Flappy_bird_game.Properties.Resources.trueFlappy;
+            this.flappy.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.flappy.Image = global::Flappy_bird_game.Properties.Resources.trueFlappy;
+            this.flappy.Location = new System.Drawing.Point(51, 145);
+            this.flappy.Name = "flappy";
+            this.flappy.Size = new System.Drawing.Size(85, 50);
+            this.flappy.TabIndex = 3;
+            this.flappy.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(168, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(336, 59);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "GAME OVER";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(160)))));
+            this.label2.Font = new System.Drawing.Font("Lucida Console", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(-2, 378);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 37);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Score:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(160)))));
+            this.label3.Font = new System.Drawing.Font("Lucida Console", 27.25F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(153, 378);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 37);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "0";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(3, 3);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Magneto", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(231, 221);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 34);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Restart";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseMove);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("Magneto", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.button2.Location = new System.Drawing.Point(384, 221);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 34);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Quit";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.MouseLeave += new System.EventHandler(this.button2_MouseLeave_1);
+            this.button2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button2_MouseMove_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.BackgroundImage = global::Flappy_bird_game.Properties.Resources.Background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(618, 415);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ground);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flappy);
+            this.Controls.Add(this.p2);
+            this.Controls.Add(this.p1);
+            this.Name = "Form1";
+            this.Text = " Flappy bird";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.flappy)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.PictureBox p1;
+        private System.Windows.Forms.PictureBox p2;
+        private System.Windows.Forms.PictureBox flappy;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Flappy bird game2/Flappy bird game/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flappy_bird_game
+{
+    public partial class Form1 : Form
+    {
+
+        public int k = 0;
+        public int score = 0;
+        public int gravity = 5;
+        public int jump = 30;
+        public int speed = 10;
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+            p1.Location = new Point(328, 0);
+            p2.Location = new Point(328, ClientSize.Height - 180);
+            label4.Text = "Speed: 10";
+            label1.Location = new Point(ClientSize.Width / 2 - label1.Width/2, ClientSize.Height / 2 - label1.Height / 2);
+            button1.Location = new Point(ClientSize.Width / 2 - button1.Width, ClientSize.Height / 2 + button1.Height);
+            button2.Location = new Point(ClientSize.Width / 2 + button2.Width / 2, ClientSize.Height / 2 + button1.Height);
+            this.DoubleBuffered = true;
+        }
+
+        //
+
+        public void Init()
+        {
+            timer1.Interval = 40;
+            timer1.Start();
+            timer2.Interval = 40;
+            timer2.Start();
+            timer3.Interval = 6000;
+            timer3.Start();
+        }
+        public void timer1_Tick(object sender, EventArgs e)
+        {
+            movePipes();
+            gameOver();
+        }
+
+        void gameOver()
+        {
+             if((flappy.Bounds.IntersectsWith(p1.Bounds)) || (flappy.Bounds.IntersectsWith(p2.Bounds)))
+            {
+                timer1.Enabled = false;
+                timer2.Enabled = false;
+                timer3.Enabled = false;
+                label1.Visible = true;
+                button1.Visible = true;
+                button1.Enabled = true;
+                button2.Visible = true;
+                button2.Enabled = true;
+            }
+
+        }
+
+        Random rand = new Random();
+
+        void movePipes()
+        {
+            if ((p1.Left >= 0) && (p2.Left >= 0))
+            {
+                p1.Left += -speed;
+                p2.Left += -speed;
+            }
+            else
+            {
+                p1.Left = 640;
+                p2.Left = 640;
+                int way = rand.Next(0, 4);
+                int x = p2.Location.X;
+                int y = p1.Location.Y;
+                switch (way)
+                {
+                    case 0:
+                        p1.Height = -1;
+                        p2.Height = 250;
+                        p2.Location = new Point(x, -50);
+                        p2.Location = new Point(x, ClientSize.Height - 312);
+                        p2.Visible = true;
+                        break;
+                    case 1:
+                        p1.Height = 250;
+                        p2.Height = -1;
+                        p2.Visible = false;
+                        break;
+                    case 2:
+                        p1.Height = 120;
+                        p2.Height = 150;
+                        p1.Location = new Point(x, -40);
+                        p2.Location = new Point(x, ClientSize.Height - 215);
+                        p2.Visible = true;
+                        break;
+                    case 3:
+                        p1.Height = 150;
+                        p2.Height = 110;
+                        p1.Location = new Point(x, 0);
+                        p2.Location = new Point(x, ClientSize.Height - 160);
+                        p2.Visible = true;
+                        break;
+
+                }
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if(flappy.Top <= ClientSize.Height-120)
+            {
+                flappy.Top += gravity;
+            }
+
+            if ((score==0)&&(flappy.Left > p1.Left + 50))
+            {
+                score++;
+            }
+
+            if (flappy.Left > p1.Left+55)
+            {
+                score++;
+            }
+            
+            label3.Text = score.ToString();
+            gameOver();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if((e.KeyCode == Keys.Space) && (flappy.Top >= flappy.Height/2) )
+            {
+                if (!(flappy.Bounds.IntersectsWith(p1.Bounds)) && !(flappy.Bounds.IntersectsWith(p2.Bounds)))
+                {
+                    flappy.Top += -jump;
+                }
+                    
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            speed++;
+            gravity++;
+            label4.Text = "Speed: ";
+            label4.Text += speed.ToString();
+            if (timer3.Interval >= 999)
+            {
+                timer3.Interval -= 50;
+            }
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            button1.BackColor = Color.Green;
+            button1.ForeColor = Color.White;
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            button1.BackColor = SystemColors.ControlLight;
+            button1.ForeColor = Color.Black;
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*score = 0;
+            gravity = 5;
+            speed = 10;
+            label4.Text = "Speed: 10";
+            label3.Text = "0";
+            label1.Visible = false;
+            button1.Visible = false;
+            button1.Enabled = false;
+            flappy.Location = new Point(51, 145);
+            p1.Location = new Point(328, -2);
+            p2.Location = new Point(328, 237);
+            p1.Height = 116;
+            p2.Height = 116;
+            timer1.Start();
+            timer2.Start();
+            timer3.Start();*/
+            Application.Restart();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_MouseLeave_1(object sender, EventArgs e)
+        {
+            button2.BackColor = SystemColors.ControlLight;
+            button2.ForeColor = Color.Black;
+        }
+
+        private void button2_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            button2.BackColor = Color.Red;
+            button2.ForeColor = Color.White;
+        }
+    }
+}
